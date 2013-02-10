63db40ef211133de39b3728b4fdab94e1c175164
Hunter <hunter@euphoria.local>
2013-02-10 02:16:53 -0800

    come on

---
+++ The Internet Archive Companion/HomeContentParentView.h
@@ -0,0 +1,13 @@
+//
+//  HomeContentParentView.h
+//  The Internet Archive Companion
+//
+//  Created by Hunter on 2/10/13.
+//  Copyright (c) 2013 Hunter Lee Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HomeContentParentView : UIView
+
+@end
---
+++ The Internet Archive Companion/HomeContentParentView.m
@@ -0,0 +1,31 @@
+//
+//  HomeContentParentView.m
+//  The Internet Archive Companion
+//
+//  Created by Hunter on 2/10/13.
+//  Copyright (c) 2013 Hunter Lee Brown. All rights reserved.
+//
+
+#import "HomeContentParentView.h"
+
+@implementation HomeContentParentView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
