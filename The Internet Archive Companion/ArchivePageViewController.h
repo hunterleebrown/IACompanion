8205685358aea7e81e45fcfb34610a5a85dd2696
Hunter <hunter@euphoria.local>
2013-02-11 22:44:18 -0800

    p

---
+++ The Internet Archive Companion/ArchivePageViewController.h
@@ -0,0 +1,13 @@
+//
+//  ArchivePageViewController.h
+//  IA
+//
+//  Created by Hunter on 2/11/13.
+//  Copyright (c) 2013 Hunter Lee Brown. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ArchivePageViewController : UIPageViewController<UIPageViewControllerDataSource>
+
+@end
