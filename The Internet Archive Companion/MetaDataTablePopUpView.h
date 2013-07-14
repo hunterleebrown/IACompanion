c8a8e738861e714d6d0712b3f734e35cb763eab1
hunterleebrown <hunter.brown@cbsinteractive.com>
2013-07-14 16:23:14 -0400

    there

---
+++ The Internet Archive Companion/MetaDataTablePopUpView.h
@@ -0,0 +1,17 @@
+//
+//  MetaDataTablePopUpView.h
+//  IA
+//
+//  Created by Hunter Brown on 7/14/13.
+//  Copyright (c) 2013 Hunter Lee Brown. All rights reserved.
+//
+
+#import "PopUpView.h"
+
+@interface MetaDataTablePopUpView : PopUpView
+
+
+- (void) showPopUpWith;
+
+
+@end
---
+++ The Internet Archive Companion/MetaDataTablePopUpView.m
@@ -0,0 +1,31 @@
+//
+//  MetaDataTablePopUpView.m
+//  IA
+//
+//  Created by Hunter Brown on 7/14/13.
+//  Copyright (c) 2013 Hunter Lee Brown. All rights reserved.
+//
+
+#import "MetaDataTablePopUpView.h"
+
+@implementation MetaDataTablePopUpView
+
+- (id)initWithFrame:(CGRect)frame
+{
+    self = [super initWithFrame:frame];
+    if (self) {
+        // Initialization code
+    }
+    return self;
+}
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect
+{
+    // Drawing code
+}
+*/
+
+@end
